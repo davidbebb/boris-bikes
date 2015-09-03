5b61f2d2615ba22a75ea39923884af2a7f70c394
davidbebb <davidbebb@gmail.com>
2015-09-03 13:31:00 +0100

    extension challenge 1

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,25 @@
 #!/usr/bin/env ruby
 
-#Where our bike code is going to be
+
 
 class DockingStation
 
+#Properties of docking station
+# - has bike
+#
+  def initialize
+    @bike_array=Array.new
+    @capacity=20
+  end
+
   def release_bike
-    #create new instance of bike
-    return Bike.new
+    fail 'No bikes here' if @bike_array.length<1
+    @bike_array.pop
+  end
 
-
+  def dock_bike(bike)
+    fail 'too many bikes here' if @bike_array.length>=@capacity
+    @bike_array<<bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,27 +4,28 @@ require "./lib/docking_station.rb"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  it "responds to a request to release a bike" do
+  it { is_expected.to respond_to :dock_bike }
+
+
+  it "releases Bike when release called" do
+    subject.instance_variable_set(:@bike_array, [Bike.new])
     expect(subject.release_bike).to be_an_instance_of Bike
   end
-  it "responds to a request to release a bike" do
-    expect(subject.release_bike.working?).to be_truthy
 
+  it "returns error when no bikes avaliable" do
+    expect{subject.release_bike}.to raise_error 'No bikes here'
+  end
+
+  it "adds bike to docking station" do
+    subject.dock_bike(Bike.new)
+    expect(subject.instance_variable_get(:@bike_array).length).to equal 1
+  end
+
+
+  it "returns error when no space for bikes avaliable" do
+    subject.instance_variable_get(:@capacity).times {subject.dock_bike Bike.new}
+    expect{subject.dock_bike(Bike.new)}.to raise_error 'too many bikes here'
   end
 
 
 end
-
-
-
-# describe DockingStation do
-#   it "returns a Docing Station object" do
-#     expect(DockingStation.new()).to be_an_instance_of DockingStation
-#   end
-#   it "responds to a request to release a bike" do
-#     expect(subject.release_bike).to be_an_instance_of Bike
-#   end
-#   it "responds to a request to release a bike" do
-#   expect(subject.release_bike.working?).to be_truthy
-#   end
-# end
